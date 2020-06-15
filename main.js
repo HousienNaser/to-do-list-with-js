f10668c47d1613a8d1318efc976cadc6c5a6dca5
HousienNaser <63602050+HousienNaser@users.noreply.github.com>
2020-06-15 19:10:35 +0300

    fix some bugs

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 function addItem(){
-/*memo = document.getElementById("newItem");
-added =document.getElementById("itemList");
-added.innerHTML="<li>"+memo.value;+"</li>"
-memo.value="";*/
+
 
 memo = document.getElementById("newItem");
 added =document.getElementById("itemList");
+
 var node = document.createElement("LI");
-  var textNode = document.createTextNode("memo.value");
-  node.appendChild(textNode);
-  added.appendChild(node);
+var textNode = document.createTextNode(memo.value);
+node.appendChild(textNode);
+added.appendChild(node);
+
   node.classList.add("notEmpty");
   memo.value="";
 }
